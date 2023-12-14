aa067e788820556fd6ce68b04433c1337755fcbb
Mitch Sutherland <mitchelsutherland@icloud.com>
2023-12-14 19:20:50 +0000

    Key functionality complete woohoo

--- script.js
+++ script.js
@@ -21,8 +21,46 @@ after player
     else if 17 < dealer < 21
         dealer stays
 check closest to 21
-*/
 
+--------------------------------------------------------------------------------
+
+/* VERSION 1 variables - expired
+
+let gameConfirmed = confirm('Would you like to play a game?');
+
+function offerHit() {
+    return confirm('Would you like to hit?')
+}
+
+/* VERSION 1 ----------------------------------------------------- Both OK and CANCEL buttons on confirm box acting is OK
+
+if (gameConfirmed) {
+    hitPlayer();
+    if (playerScore == 21) {
+        alert('Player wins!');
+    } else {
+        console.log(`You now have ${playerScore}`)
+        dealerScore += newCard(2, 11);
+        console.log(`The dealer has ${dealerScore}`)
+    }
+}
+
+while (playerScore > 0 && playerScore < 21) {
+    offerHit();
+    if (true) {
+        hitPlayerLow();
+        if (playerScore == 21) {
+                alert(`Player wins. You have ${playerScore}!`);
+            } else if (playerScore > 21) {
+                alert(`Player loses with ${playerScore}`);
+            } else {
+                console.log(`You now have ${playerScore}`);
+            }
+        } else {
+            alert(`Player ends with ${playerScore}`)
+        }
+        ---------------------------------------------------------------
+    */
 
 // -----------------------------------------------------------------------------
 
@@ -41,14 +79,6 @@ const hitPlayerLow = function() {
     playerScore += newCard(4, 11);
 }
 
-// console.log(newCard(4, 21))
-
-let gameConfirmed = confirm('Would you like to play a game?');
-
-function offerHit() {
-    return confirm('Would you like to hit?')
-}
-
 function dealerPlay() {
     dealerScore += newCard(4, 11);
     alert(`The dealer now has ${dealerScore}`);
@@ -65,42 +95,13 @@ function dealerPlay() {
     } else {
         alert(`The dealer has bust with ${dealerScore}`)
         console.log(`The dealer has bust with ${dealerScore}`)
-        }
+    }
 }
 
-
 function playgame() {
-    /* VERSION 1 ----------------------------------------------------- Both OK and CANCEL buttons on confirm box acting is OK
-    if (gameConfirmed) {
-        hitPlayer();
-        if (playerScore == 21) {
-            alert('Player wins!');
-        } else {
-            console.log(`You now have ${playerScore}`)
-            dealerScore += newCard(2, 11);
-            console.log(`The dealer has ${dealerScore}`)
-        }
-    }
 
-        while (playerScore > 0 && playerScore < 21) {
-        offerHit();
-        if (true) {
-            hitPlayerLow();
-            if (playerScore == 21) {
-                alert(`Player wins. You have ${playerScore}!`);
-            } else if (playerScore > 21) {
-                alert(`Player loses with ${playerScore}`);
-            } else {
-                console.log(`You now have ${playerScore}`);
-            }
-        } else {
-            alert(`Player ends with ${playerScore}`)
-        }
-        ---------------------------------------------------------------
-    */
+    // VERSION 2 ----------------------------------------------------- 
 
-
-    // VERSION 2 ----------------------------------------------------- Each confirm pop up is repeating twice
     if (confirm('Would you like to play a game?') == true) {
         hitPlayer();
         if (playerScore == 21) {
@@ -130,59 +131,11 @@ function playgame() {
                 break;
             }
         }
-    //---------------------------------------------------------
-
-
+    //---------------------------------------------------------------
 
 }
 
 console.log(playgame())
 
-
-/* BROKEN CODE - INCLUDES PREVIOUS ATTEMPTS 
-
-    while (playerScore < 21) {
-        offerHit();
-        if (true) {
-            hitPlayerLow();
-            if (playerScore == 21) {
-                alert(`Player wins. You have ${playerScore}!`);
-            } else if (playerScore > 21) {
-                alert(`Player loses with ${playerScore}`);
-            } else {
-                console.log(`You now have ${playerScore}`);
-            }
-        }
-        
-        if (false) {
-            alert(`Player ends with ${playerScore}`)
-            // Attempted to address this in the version below but not currently working.
-        }
-    }
-}
-
-
-
-/* REVIEW THIS WITH TA TO UNDERSTAND WHY IT WILL NOT WORK AND/OR HOW TO INCORPORATE CANCEL BUTTON FROM CONFIRM BOX (FALSE) TO EXECUTE.
-
-while (playerScore < 21) {
-    offerHit();
-
-    if (offerHit() === true) {
-        hitPlayer(4, 11);
-        if (playerScore == 21) {
-            alert('Player wins!');
-        } else if (playerScore > 21) {
-            alert(`Player loses with ${playerScore}`)
-        } else {
-            console.log(`You now have ${playerScore}`)
-        }
-    }
-    
-    if (offerHit() === false) {
-        alert(`Player ends with ${playerScore}`)
-    }
-}
-*/
 
 
